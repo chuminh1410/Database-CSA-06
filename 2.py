6118941cb7402f0781de4cf84863ddcff98bde8d
chuminh1410 <chungocquangminh@gmail.com>
2023-10-07 20:59:01 +0700

    1

--- 2.py
+++ 2.py
@@ -1,14 +1,15 @@
 import sqlite3
-conn = sqlite3.connect("quan_ly_sinh_vien.db")
+conn = sqlite3.connect("BangDiemSV.db")
 c=conn.cursor()
 c.execute("""
     create table sinh_vien(
-            ma_sv int,
+            ID int,
             ten varchar,
-            nam_sinh varchar,
             dia_chi varchar,
-            lop_hoc varchar,
-            diem_toan varchar,
-            primary key(ma_sv)
+            nam_sinh varchar,
+            toan int,
+            ly int,
+            hoa int,
+            primary key(ID)
     )          
 """)
